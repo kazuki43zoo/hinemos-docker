5524868756b5ff393d780dad1f8d5c927c2312a0
Kazuki Shimizu <kazuki43zoo@gmail.com>
2024-09-22 21:01:31 +0900

    Add controller

--- agent-with-java/src/main/java/com/example/demo/DemoApplication.java
+++ agent-with-java/src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,14 @@
 package com.example.demo;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +16,16 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+  @RestController
+  public static class HelloController {
+    private static Logger logger = LoggerFactory.getLogger(HelloController.class);
+    @GetMapping("/")
+    public String hello() throws UnknownHostException {
+      String hostname = InetAddress.getLocalHost().getHostName();
+      logger.info("hello on {}", hostname);
+      return "hello on " + hostname;
+    }
+  }
 
 }
