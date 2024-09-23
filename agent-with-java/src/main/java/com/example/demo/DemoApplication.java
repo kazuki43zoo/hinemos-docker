dbef62206f7e8615d077a6519e50429347359f9b
Kazuki Shimizu <kazuki43zoo@gmail.com>
2024-09-23 10:27:52 +0900

    Add controller for view file

--- agent-with-java/src/main/java/com/example/demo/DemoApplication.java
+++ agent-with-java/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -25,6 +32,11 @@ public class DemoApplication {
       String hostname = InetAddress.getLocalHost().getHostName();
       logger.info("hello on {}", hostname);
       return "hello on " + hostname;
+    }
+    @GetMapping("/file")
+    public ResponseEntity<String> viewFile(@RequestParam(name = "path", defaultValue = "/var/log/demo-console.log") String path) throws IOException {
+      Resource logFile = new FileSystemResource(path);
+      return logFile.exists() ? ResponseEntity.ok(StreamUtils.copyToString(logFile.getInputStream(), StandardCharsets.UTF_8)) : ResponseEntity.notFound().build();
     }
   }
 
